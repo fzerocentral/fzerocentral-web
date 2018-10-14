d72488f87529c654af7fb092da07b7be5300270c
yoshifan <yoshifan@users.noreply.github.com>
2018-10-14 11:43:55 -0700

    Use camelCase attributes instead of underscored in the Record model. Still need to fix other models the same way.

--- app/models/record.js
+++ app/models/record.js
@@ -1,11 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  value: DS.attr(),
-  achieved_at: DS.attr(),
-  is_improvement: DS.attr(),
+  value: DS.attr('number'),
+  achievedAt: DS.attr('date'),
+  isImprovement: DS.attr(),
   rank: DS.attr(),
-  value_display: DS.attr(),
+  valueDisplay: DS.attr(),
 
   chart: DS.belongsTo('chart'),
   user: DS.belongsTo('user'),
