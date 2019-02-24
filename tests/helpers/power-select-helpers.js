312e56a76fc95457efe2a1393f515fb9d4ee68d5
yoshifan <yoshifan@users.noreply.github.com>
2019-02-24 01:47:55 -0800

    filter-apply-form component now has fairly thorough tests.

---
+++ tests/helpers/power-select-helpers.js
@@ -0,0 +1,31 @@
+import { click } from '@ember/test-helpers';
+
+export async function assertPowerSelectOptionsEqual(
+    assert, powerSelectElement, expectedOptions, assertMessage) {
+  // Note: This function has to click the power select trigger, and may thus
+  // interfere with interactivity on other elements.
+  //
+  // To check a power-select's options in the HTML, we have to first click
+  // the power-select to make the dropdown appear, then we have to check
+  // the ember-basic-dropdown-wormhole for the element indicated by the
+  // power-select's aria-owns attribute.
+  await click(`#${powerSelectElement.id}`);
+  let dropdownContentId = powerSelectElement.getAttribute('aria-owns');
+  let dropdownContent = document.getElementById(dropdownContentId);
+  let optionsList =
+    dropdownContent.querySelector('ul.ember-power-select-options');
+  let optionTexts =
+    Array.from(optionsList.querySelectorAll('li'))
+    .map(element => element.textContent.trim());
+  assert.deepEqual(optionTexts, expectedOptions, assertMessage);
+}
+
+export function assertPowerSelectCurrentTextEqual(
+    assert, powerSelectElement, expectedText, assertMessage) {
+  assert.equal(
+    powerSelectElement
+      .querySelector(`span:first-child`)
+      .textContent.trim(),
+    expectedText,
+    assertMessage);
+}
--- tests/integration/components/filter-apply-form-test.js
+++ tests/integration/components/filter-apply-form-test.js
@@ -1,9 +1,41 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { A } from '@ember/array';
+import { click, render } from '@ember/test-helpers';
 import { run } from "@ember/runloop";
-import { startMirage } from 'fzerocentral-web/initializers/ember-cli-mirage';
 import hbs from 'htmlbars-inline-precompile';
+import { selectChoose } from 'ember-power-select/test-support';
+import { startMirage } from 'fzerocentral-web/initializers/ember-cli-mirage';
+import { assertPowerSelectOptionsEqual, assertPowerSelectCurrentTextEqual }
+  from 'fzerocentral-web/tests/helpers/power-select-helpers';
+
+function getFilterGroupSelect(testModule) {
+  return testModule.element.querySelectorAll(`.ember-power-select-trigger`)[0];
+}
+
+function getCompareMethodSelect(testModule) {
+  return testModule.element.querySelectorAll(`.ember-power-select-trigger`)[1];
+}
+
+function getFilterSelect(testModule) {
+  return testModule.element.querySelectorAll(`.ember-power-select-trigger`)[2];
+}
+
+function createModelInstance(server, modelName, args) {
+  return server.create(modelName, args);
+}
+
+function modelAsProperty(store, modelName, modelRecord) {
+  // The result of server.create() is good for passing into another create()
+  // call to specify a model relationship.
+  // However, the result of server.create() doesn't have a function get(), so
+  // it can't be passed as a component property. Need to call findRecord() to
+  // get our hands on an object with get() available.
+  //
+  // Pass a server.create() result to this function as modelRecord, and this
+  // function will return an object with get().
+  return run(() => store.findRecord(modelName, modelRecord.id));
+}
 
 module('Integration | Component | filter-apply-form', function(hooks) {
   setupRenderingTest(hooks);
@@ -12,27 +44,217 @@ module('Integration | Component | filter-apply-form', function(hooks) {
     this.server = startMirage();
     this.store = this.owner.lookup('service:store');
 
-    this.groupA = server.create('filterGroup', {name: 'Group A'});
-    this.filterA1 = server.create('filter', {name: 'Filter A1', filterGroup: this.groupA});
-    this.filterA2 = server.create('filter', {name: 'Filter A2', filterGroup: this.groupA});
+    this.machineGroup = createModelInstance(
+      this.server, 'filterGroup',
+      {name: 'Machine', kind: 'select'});
+    this.blueFalconFilter = createModelInstance(
+      this.server, 'filter',
+      {name: 'Blue Falcon', filterGroup: this.machineGroup});
+    this.whiteCatFilter = createModelInstance(
+      this.server, 'filter',
+      {name: 'White Cat', filterGroup: this.machineGroup});
+
+    this.settingGroup = createModelInstance(
+      this.server, 'filterGroup',
+      {name: 'Setting', kind: 'numeric'});
+    this.setting30Filter = createModelInstance(
+      this.server, 'filter',
+      {name: '30%', numericValue: 30, filterGroup: this.settingGroup});
+    this.setting80Filter = createModelInstance(
+      this.server, 'filter',
+      {name: '80%', numericValue: 80, filterGroup: this.settingGroup});
+
+    this.machineGroupAsProperty =
+      modelAsProperty(this.store, 'filterGroup', this.machineGroup);
+    this.settingGroupAsProperty =
+      modelAsProperty(this.store, 'filterGroup', this.settingGroup);
+
+    // Individual tests can set their property values; these just act as
+    // defaults.
+    this.set(
+      'filterGroups',
+      A([this.machineGroupAsProperty, this.settingGroupAsProperty]));
+    this.set('appliedFiltersString', null);
+    this.set(
+      'updateAppliedFiltersString',
+      (s) => this.set('appliedFiltersString', s));
   });
 
-  test('it renders', async function(assert) {
-    this.set('filterGroups', run(() => this.store.findAll('filterGroup')));
-    this.set('appliedFiltersString', `${this.filterA1.id}`);
-    await render(hbs`{{filter-apply-form filterGroups=filterGroups appliedFiltersString=appliedFiltersString}}`);
+  hooks.afterEach( function() {
+    this.server.shutdown();
+  });
 
-    // textContent has a lot of newlines and extra spaces on either side of
-    // them. We'll get rid of those before comparing.
-    let textContentLines = [];
-    this.element.textContent.split('\n').forEach((rawLine) => {
-      let line = rawLine.trim();
-      if (line !== '') {
-        textContentLines.push(line);
-      }
-    })
-    assert.deepEqual(textContentLines, [
-      "Add filter:", "Not selected", "Not selected", "Not selected", "Add",
-      "Applied filters:", "Group A: Filter A1", "x"]);
+  test("first dropdown has the filter groups", async function(assert) {
+    await render(
+      hbs`{{filter-apply-form filterGroups=filterGroups
+            appliedFiltersString=appliedFiltersString}}`);
+
+    let filterGroupSelect = getFilterGroupSelect(this);
+    await assertPowerSelectOptionsEqual(
+      assert, filterGroupSelect, ["Machine", "Setting"],
+      "Options are as expected");
+    assertPowerSelectCurrentTextEqual(
+      assert, filterGroupSelect, "Not selected",
+      "Default selection is as expected");
+  });
+
+  test("selecting a filter group fills the second dropdown with relevant compare options", async function(assert) {
+    await render(
+      hbs`{{filter-apply-form filterGroups=filterGroups
+            appliedFiltersString=appliedFiltersString}}`);
+
+    let filterGroupSelect = getFilterGroupSelect(this);
+    let compareMethodSelect = getCompareMethodSelect(this);
+
+    await selectChoose(`#${filterGroupSelect.id}`, 'Machine');
+    await assertPowerSelectOptionsEqual(
+      assert, compareMethodSelect, ["-", "NOT"],
+      "Choice-based filter group: options are as expected");
+    assertPowerSelectCurrentTextEqual(
+      assert, compareMethodSelect, "-",
+      "Choice-based filter group: default selection as expected");
+
+    // Numeric filter group
+    await selectChoose(`#${filterGroupSelect.id}`, 'Setting');
+    await assertPowerSelectOptionsEqual(
+      assert, compareMethodSelect, ["-", "NOT", ">=", "<="],
+      "Numeric filter group: options are as expected");
+    assertPowerSelectCurrentTextEqual(
+      assert, compareMethodSelect, "-",
+      "Numeric filter group: default selection as expected");
+  });
+
+  test("selecting a filter group fills the third dropdown with the group's filter options", async function(assert) {
+    await render(
+      hbs`{{filter-apply-form filterGroups=filterGroups
+            appliedFiltersString=appliedFiltersString}}`);
+
+    let filterGroupSelect = getFilterGroupSelect(this);
+    let filterSelect = getFilterSelect(this);
+
+    await selectChoose(`#${filterGroupSelect.id}`, 'Machine');
+    await assertPowerSelectOptionsEqual(
+      assert, filterSelect, ["Blue Falcon", "White Cat"],
+      "Choice-based filter group: options are as expected");
+    assertPowerSelectCurrentTextEqual(
+      assert, filterSelect, "Blue Falcon",
+      "Choice-based filter group: default selection as expected");
+
+    await selectChoose(`#${filterGroupSelect.id}`, 'Setting');
+    await assertPowerSelectOptionsEqual(
+      assert, filterSelect, ["30%", "80%"],
+      "Numeric filter group: options are as expected");
+    assertPowerSelectCurrentTextEqual(
+      assert, filterSelect, "30%",
+      "Numeric filter group: default selection as expected");
+  });
+
+  test("adding a filter calls updateAppliedFiltersString with an appropriate updated filters string", async function(assert) {
+    await render(
+      hbs`{{filter-apply-form filterGroups=filterGroups
+            appliedFiltersString=appliedFiltersString
+            updateAppliedFiltersString=updateAppliedFiltersString}}`);
+
+    let filterGroupSelect = getFilterGroupSelect(this);
+    let compareMethodSelect = getCompareMethodSelect(this);
+    let filterSelect = getFilterSelect(this);
+
+    await selectChoose(`#${filterGroupSelect.id}`, 'Machine');
+    await selectChoose(`#${compareMethodSelect.id}`, '-');
+    await selectChoose(`#${filterSelect.id}`, 'White Cat');
+    await click(`button.add-filter-button`);
+    let expectedAFString = `${this.whiteCatFilter.id}`;
+    assert.equal(
+      this.get('appliedFiltersString'), expectedAFString,
+      "Works for equality comparison");
+
+    await selectChoose(`#${filterGroupSelect.id}`, 'Machine');
+    await selectChoose(`#${compareMethodSelect.id}`, 'NOT');
+    await selectChoose(`#${filterSelect.id}`, 'Blue Falcon');
+    await click(`button.add-filter-button`);
+    expectedAFString += `-${this.blueFalconFilter.id}n`;
+    assert.equal(
+      this.get('appliedFiltersString'), expectedAFString,
+      "Works for NOT comparison");
+
+    await selectChoose(`#${filterGroupSelect.id}`, 'Setting');
+    await selectChoose(`#${compareMethodSelect.id}`, '>=');
+    await selectChoose(`#${filterSelect.id}`, '80%');
+    await click(`button.add-filter-button`);
+    expectedAFString += `-${this.setting80Filter.id}ge`;
+    assert.equal(
+      this.get('appliedFiltersString'), expectedAFString,
+      "Works for greater-or-equal comparison");
+
+    await selectChoose(`#${filterGroupSelect.id}`, 'Setting');
+    await selectChoose(`#${compareMethodSelect.id}`, '<=');
+    await selectChoose(`#${filterSelect.id}`, '30%');
+    await click(`button.add-filter-button`);
+    expectedAFString += `-${this.setting30Filter.id}le`;
+    assert.equal(
+      this.get('appliedFiltersString'), expectedAFString,
+      "Works for less-or-equal comparison");
+  });
+
+  test("each part of appliedFiltersString gets a human-readable display after 'Applied filters:'", async function(assert) {
+    let appliedFiltersString =
+      `${this.whiteCatFilter.id}`
+      + `-${this.blueFalconFilter.id}n`
+      + `-${this.setting80Filter.id}ge`
+      + `-${this.setting30Filter.id}le`;
+    this.set('appliedFiltersString', appliedFiltersString);
+    await render(
+      hbs`{{filter-apply-form filterGroups=filterGroups
+            appliedFiltersString=appliedFiltersString
+            updateAppliedFiltersString=updateAppliedFiltersString}}`);
+
+    let appliedFilterDisplays =
+      Array.from(this.element.querySelectorAll('span.applied-filter'))
+      .map(element => element.textContent.trim());
+    let expectedAFDisplays = [
+      "Machine: White Cat", "Machine: NOT Blue Falcon",
+      "Setting: >= 80%", "Setting: <= 30%"];
+    assert.deepEqual(
+      appliedFilterDisplays, expectedAFDisplays,
+      "Displayed text is as expected");
+  });
+
+  test("clicking the remove button next to an applied-filter display removes that filter, calling updateAppliedFiltersString() with the appropriate new string", async function(assert) {
+    let appliedFiltersString =
+      `${this.whiteCatFilter.id}`
+      + `-${this.blueFalconFilter.id}n`
+      + `-${this.setting30Filter.id}le`;
+    this.set('appliedFiltersString', appliedFiltersString);
+    await render(
+      hbs`{{filter-apply-form filterGroups=filterGroups
+            appliedFiltersString=appliedFiltersString
+            updateAppliedFiltersString=updateAppliedFiltersString}}`);
+
+    let removeButtons =
+      this.element.querySelectorAll('button.applied-filter-remove-button');
+    await click(removeButtons[1]);
+    let expectedAFString =
+      `${this.whiteCatFilter.id}`
+      + `-${this.setting30Filter.id}le`;
+    assert.equal(
+      this.get('appliedFiltersString'), expectedAFString,
+      "Properly updated after 1st removal");
+
+    removeButtons =
+      this.element.querySelectorAll('button.applied-filter-remove-button');
+    await click(removeButtons[0]);
+    expectedAFString =
+      `${this.setting30Filter.id}le`;
+    assert.equal(
+      this.get('appliedFiltersString'), expectedAFString,
+      "Properly updated after 2nd removal");
+
+    removeButtons =
+      this.element.querySelectorAll('button.applied-filter-remove-button');
+    await click(removeButtons[0]);
+    expectedAFString = null;
+    assert.equal(
+      this.get('appliedFiltersString'), expectedAFString,
+      "Properly updated after 3rd removal");
   });
 });
