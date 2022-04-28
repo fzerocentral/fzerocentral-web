0c8ce5c6f17a5288e562ff4c4a94b3fa31d70d9a
JoselleAstrid <JoselleAstrid@users.noreply.github.com>
2022-04-28 05:32:53 -0700

    Recreate most of FZC's site header.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,13 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 import config from '../config/environment';
 
 
 /* This is the controller for templates/application.hbs. */
 export default class ApplicationController extends Controller {
+  @service router;
+
   devMode = config.APP.devMode;
+  indexUrl = this.router.urlFor('index');
+  rootURL = config.rootURL;
 }
