54e8b16a6473b7175cf4083db5f44bd621242330
JoselleAstrid <JoselleAstrid@users.noreply.github.com>
2022-04-28 21:00:16 -0700

    Recreate some of FZC's front page. Clean up ember-tutorial.css.

---
+++ app/controllers/index.js
@@ -0,0 +1,7 @@
+import Controller from '@ember/controller';
+import config from '../config/environment';
+
+
+export default class IndexController extends Controller {
+  rootURL = config.rootURL;
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {
-  beforeModel() {
-    this.replaceWith('games');
-  }
-}
+export default class IndexRoute extends Route {}
