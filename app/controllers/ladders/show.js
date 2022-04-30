f8d349d0302e72384cc886adefe1198db68426b0
JoselleAstrid <JoselleAstrid@users.noreply.github.com>
2022-04-29 17:18:57 -0700

    ladders/show: Add the same kind of 'ladder filters' display that charts/show has. Also change the color of that display.

--- app/controllers/ladders/show.js
+++ app/controllers/ladders/show.js
@@ -1,9 +1,15 @@
 import Controller from '@ember/controller';
+import { filterSpecStrToDisplays } from "../../utils/filter-specs";
 
 
 export default class LaddersShowController extends Controller {
 
   get firstEntry() {
     return this.model.ranking.objectAt(0);
+  }
+
+  get ladderFilterDisplays() {
+    return filterSpecStrToDisplays(
+      this.model.ladder.filterSpec, this.model.ladderFilterObjs);
   }
 }
