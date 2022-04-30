55048ff9c51b9733732ce5ff94c4b2e67370bb15
Ricardo Mendes <rokusu@gmail.com>
2022-04-30 06:43:42 +0100

    v3.17.0...v3.28.5

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,11 +1,14 @@
-import Application from '../app';
-import config from '../config/environment';
-import setupSinon from 'ember-sinon-qunit';
+import Application from 'fzerocentral-web/app';
+import config from 'fzerocentral-web/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import setupSinon from 'ember-sinon-qunit';
 
 setApplication(Application.create(config.APP));
 
+setup(QUnit.assert);
 setupSinon();
 
 start();
